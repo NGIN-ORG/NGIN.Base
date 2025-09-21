e2b0c4616e91815db243bca7643bd271a0f3935c
Maximiliam Berggren <berggrenmille@hotmail.se>
2025-09-21 21:11:23 +0200

    Improve code formatting and variable naming consistency in UnitsExample

--- examples/UnitsExample/main.cpp
+++ examples/UnitsExample/main.cpp
@@ -9,14 +9,14 @@ int main()
 
     using CustomUnit = Unit<LENGTH, double, RatioPolicy<1, 873758>>;
     // Basic construction and output
-    Meters m(5.0);
-    Kilometers km(1.2);
-    Seconds s(10.0);
+    Meters       m(5.0);
+    Kilometers   km(1.2);
+    Seconds      s(10.0);
     Milliseconds ms(500.0);
-    Celsius c(25.0);
-    Kelvin k   = UnitCast<Kelvin>(c);
-    Celsius c2 = UnitCast<Celsius>(k);
-    CustomUnit customUnit(3.0);
+    Celsius      c(25.0);
+    Kelvin       k  = UnitCast<Kelvin>(c);
+    Celsius      c2 = UnitCast<Celsius>(k);
+    CustomUnit   customUnit(3.0);
 
     std::cout << "Meters: " << m << std::endl;
     std::cout << "Kilometers: " << km << std::endl;
@@ -27,7 +27,7 @@ int main()
     std::cout << "Kelvin to Celsius: " << c2 << std::endl;
 
     // Arithmetic
-    auto total_m = m + UnitCast<Meters>(Kilometers(1.0));
+    auto totalM = m + UnitCast<Meters>(Kilometers(1.0));
     std::cout << "Total meters (5m + 1km): " << total_m << std::endl;
 
     auto speed = m / s;
@@ -35,7 +35,7 @@ int main()
 
     // Affine conversion test
     Celsius freezing(0.0);
-    Kelvin freezingK = UnitCast<Kelvin>(freezing);
+    Kelvin  freezingK = UnitCast<Kelvin>(freezing);
     std::cout << "Freezing point: " << freezing << " = " << freezingK << std::endl;
 
     // Custom unit output
@@ -43,7 +43,7 @@ int main()
 
     /// Formatting example
     Seconds t {3.5};
-    Meters d {12.0};
+    Meters  d {12.0};
     std::println("Speed: {:.1f}", (d / t));// prints “Speed: 3.4 m/s”
 
     return 0;
